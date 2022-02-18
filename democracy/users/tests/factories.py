1d5bbdb6ec4effa609e20f6e88c968b258addcd8
Ryan Fleck <Ryan.Fleck@protonmail.com>
2022-02-18 11:48:36 -0700

    Ignore "deprecated in Django 4" warnings

--- democracy/users/tests/factories.py
+++ democracy/users/tests/factories.py
@@ -14,16 +14,7 @@ class UserFactory(DjangoModelFactory):
     @post_generation
     def password(self, create: bool, extracted: Sequence[Any], **kwargs):
         locale = "en_US"
-        faked_password = Faker(
-            "password",
-            length=42,
-            special_chars=True,
-            digits=True,
-            upper_case=True,
-            lower_case=True,
-            locale=locale,
-        ).generate({"locale": locale})
-
+        faked_password = "as9df79a8s7df9..!!$$(*)%)(*^a8s7df98as7dffdsfa89a9s8fd"
         self.set_password(extracted if extracted else faked_password)
 
     class Meta:
